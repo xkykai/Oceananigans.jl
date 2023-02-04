07b5de468e01a51f4f6313cb7e07b4ebf471abfd
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2023-02-04 13:28:22 -0500

    revert my changes

--- validation/immersed_boundaries/complex_domain_convection.jl
+++ validation/immersed_boundaries/complex_domain_convection.jl
@@ -2,7 +2,6 @@ using Oceananigans
 using GLMakie
 using Printf
 
-
 include("immersed_pressure_solver.jl")
 
 #####
@@ -52,12 +51,6 @@ compute!(B)
 compute!(C)
 B₀ = B[1, 1, 1]
 C₀ = C[1, 1, 1]
-
-c = model.tracers.c
-C = Field(Integral(c))
-compute!(C)
-C₀ = C[1, 1, 1]
-
 
 function progress(sim)
     elapsed = time_ns() - wall_time[]
@@ -115,7 +108,6 @@ filename = prefix * "_fields.jld2"
 bt = FieldTimeSeries(filename, "b")
 ct = FieldTimeSeries(filename, "c")
 wt = FieldTimeSeries(filename, "w")
-ct = FieldTimeSeries(filename, "c")
 pt = FieldTimeSeries(filename, "p")
 δt = FieldTimeSeries(filename, "δ")
 times = bt.times
@@ -125,27 +117,17 @@ time_series_filename = prefix * "_time_series.jld2"
 Bt = FieldTimeSeries(time_series_filename, "B")
 Ct = FieldTimeSeries(time_series_filename, "C")
 
-##
-fig = Figure(resolution=(1700, 1000))
+fig = Figure(resolution=(1200, 1200))
 
-slider = Slider(fig[0, 1:4], range=1:Nt, startvalue=1)
+slider = Slider(fig[0, 1:3], range=1:Nt, startvalue=1)
 n = slider.value
 
-B₀ = interior(Bt[1], 1, 1, 1)
-C₀ = interior(Ct[1], 1, 1, 1)
-
+B₀ = sum(interior(bt[1], :, 1, :)) / (Nx * Nz)
 btitlestr = @lift @sprintf("Buoyancy at t = %.2f", times[$n])
 wtitlestr = @lift @sprintf("Vertical velocity at t = %.2f", times[$n])
-ctitlestr = @lift @sprintf("Passive tracer at t = %.2f", times[$n])
-Δctitlestr = @lift @sprintf("Passive tracer anomaly at t = %.2f", times[$n])
 
 axb = Axis(fig[1, 1], title=btitlestr)
 axw = Axis(fig[1, 2], title=wtitlestr)
-axc = Axis(fig[1, 3], title=ctitlestr)
-axΔc = Axis(fig[1, 4], title=Δctitlestr)
-
-axbt = Axis(fig[2, 1:2], xlabel="Time", ylabel="(∫b - ∫b₀) / ∫b₀", title="Total buoyancy anomaly")
-axct = Axis(fig[2, 3:4], xlabel="Time", ylabel="(∫c - ∫c₀) / ∫c₀", title="Total passive tracer anomaly")
 axc = Axis(fig[1, 3], title="Passive tracer anomaly")
 axp = Axis(fig[2, 1], title="Pressure")
 axd = Axis(fig[2, 2], title="Divergence")
@@ -154,29 +136,10 @@ axt = Axis(fig[3, 1:3], xlabel="Time", ylabel="Fractional remaining tracer")
 bn = @lift interior(bt[$n], :, 1, :)
 c′n = @lift interior(ct[$n], :, 1, :) .- interior(ct[1], :, 1, :)
 wn = @lift interior(wt[$n], :, 1, :)
-cn = @lift interior(ct[$n], :, 1, :)
-Δcn = @lift interior(ct[$n], :, 1, :) .- interior(ct[1], :, 1, :)
 pn = @lift interior(pt[$n], :, 1, :)
 δn = @lift interior(δt[$n], :, 1, :)
 
-blim = 0.2
 wlim = maximum(abs, wt) / 2
-clim = maximum(abs, ct) / 10
-Δclim = maximum(abs, ct) / 10
-
-heatmap!(axb, bn, colormap=:balance, colorrange=(-blim, blim))
-heatmap!(axw, wn, colormap=:balance, colorrange=(-wlim, wlim))
-heatmap!(axc, cn, colormap=:balance, colorrange=(-clim, clim))
-heatmap!(axΔc, Δcn, colormap=:balance, colorrange=(-Δclim, Δclim))
-
-ΔB = (Bt.data[1, 1, 1, :] .- Bt.data[1, 1, 1, 1]) ./ Bt.data[1, 1, 1, 1]
-t = @lift times[$n]
-lines!(axbt, Bt.times, ΔB)
-vlines!(axbt, t)
-
-ΔC = (Ct.data[1, 1, 1, :] .- Ct.data[1, 1, 1, 1]) ./ Ct.data[1, 1, 1, 1]
-lines!(axct, Ct.times, ΔC)
-vlines!(axct, t)
 clim = 1e-1
 δlim = 1e-14
 
@@ -194,9 +157,8 @@ lines!(axt, Ct.times, ΔC)
 vlines!(axt, t)
 
 display(fig)
-##
 
-record(fig, prefix * ".mp4", 1:Nt, framerate=12) do nn
-    @info string("Plotting frame ", nn, " of ", Nt)
-    n[] = nn
-end
+# record(fig, prefix * ".mp4", 1:Nt, framerate=12) do nn
+#     @info string("Plotting frame ", nn, " of ", Nt)
+#     n[] = nn
+# end
